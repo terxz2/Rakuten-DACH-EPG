df110eda5a49b25dfefa52a6598b557d0b0156e2
terxz2 <chug-straw-splashy@duck.com>
2026-06-18 18:26:00 +0200

    Simplify get_days function by removing extra days
    Removed unused day calculations from get_days function.

--- generate_de.py
+++ generate_de.py
@@ -17,12 +17,13 @@ def get_days() -> list:
     now = datetime.now().replace(hour=(datetime.now()).hour, minute=0, second=0, microsecond=0)
     day_1 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(1))
     day_2 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(2))
-    day_3 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(3))
-    day_4 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(4))
-    day_5 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(5))
-    day_6 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(6))
-    day_7 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(7))
-    return [now, day_1, day_2, day_3, day_4, day_5, day_6, day_7]
+#    day_3 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(3))
+#    day_4 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(4))
+#    day_5 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(5))
+#    day_6 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(6))
+#    day_7 = (datetime.combine(datetime.now(), time(0, 0)) + timedelta(7))
+#    return [now, day_1, day_2, day_3, day_4, day_5, day_6, day_7]
+     return [now, day_1, day_2]
 
 
 def build_xmltv(channels: list, programmes: list) -> bytes:
